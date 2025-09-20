de676f5adfb04fed56726481f68b088434e8f88a
jeremyL88 <jeremy.lee-1@student.uts.edu.au>
2025-09-20 22:15:45 +1000

    animator controller working with tweening

--- Assets/Scripts/Knight_Movement.cs
+++ Assets/Scripts/Knight_Movement.cs
@@ -6,40 +6,52 @@ public class Knight_Movement : MonoBehaviour
 {
     [SerializeField]
     private GameObject character;
+    [SerializeField]
+    private Animator animator;
     private Tweener tweener;
+
+    private Vector3 topLeft = new Vector3(16f, 20f, -0.2f);
+    private Vector3 topRight = new Vector3(21f, 20f, -0.2f);
+    private Vector3 bottomRight = new Vector3(21f, 16f, -0.2f);
+    private Vector3 bottomLeft = new Vector3(16f, 16f, -0.2f);
     // Start is called before the first frame update
     void Start()
     {
         tweener = GetComponent<Tweener>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 startPos = character.transform.position;
-        if (Input.GetKeyDown("a"))
+        if (startPos == topLeft)
         {
-            Vector3 endPos = new Vector3(-2.0f, 0.5f, 0.0f);
-            float duration = 1.5f;
+            Vector3 endPos = new Vector3(21f, 20f, -0.2f);
+            float duration = 1.25f;
             tweener.AddTween(character.transform, startPos, endPos, duration);
+            animator.SetTrigger("MoveRight");
         }
-        if (Input.GetKeyDown("d"))
+        if (startPos == topRight)
         {
-            Vector3 endPos = new Vector3(2.0f, 0.5f, 0.0f);
-            float duration = 1.5f;
+            Vector3 endPos = new Vector3(21f, 16f, -0.2f);
+            float duration = 1f;
             tweener.AddTween(character.transform, startPos, endPos, duration);
+            animator.SetTrigger("MoveDown");
         }
-        if (Input.GetKeyDown("s"))
+        if (startPos == bottomRight)
         {
-            Vector3 endPos = new Vector3(0.0f, 0.5f, -2.0f);
-            float duration = 0.5f;
+            Vector3 endPos = new Vector3(16f, 16f, -0.2f);
+            float duration = 1.25f;
             tweener.AddTween(character.transform, startPos, endPos, duration);
+            animator.SetTrigger("MoveLeft");
         }
-        if (Input.GetKeyDown("w"))
+        if (startPos == bottomLeft)
         {
-            Vector3 endPos = new Vector3(0.0f, 0.5f, 2.0f);
-            float duration = 0.5f;
+            Vector3 endPos = new Vector3(16f, 20f, -0.2f);
+            float duration = 1f;
             tweener.AddTween(character.transform, startPos, endPos, duration);
+            animator.SetTrigger("MoveUp");
         }
     }
 }
