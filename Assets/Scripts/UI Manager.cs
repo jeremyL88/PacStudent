d3d904d9155e283cdbdc592a04e76774f2ce3d24
jeremyL88 <jeremy.lee-1@student.uts.edu.au>
2025-10-25 18:02:53 +1100

    button functionality

---
+++ Assets/Scripts/UI Manager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene(0);
+        
+    }
+}
