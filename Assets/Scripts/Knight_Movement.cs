fb08abecac3021fab251a5e18b65cdb32a639feb
jeremyL88 <jeremy.lee-1@student.uts.edu.au>
2025-10-28 23:31:30 +1100

    coin sound

--- Assets/Scripts/Knight_Movement.cs
+++ Assets/Scripts/Knight_Movement.cs
@@ -10,7 +10,8 @@ public class Knight_Movement : MonoBehaviour
     [SerializeField]
     private Animator animator;
     private Tweener tweener;
-    public new AudioSource audio;
+    public new AudioSource footsteps;
+    public new AudioSource coin;
 
     private Vector3 pastPosition;
     private string lastInput;
@@ -59,6 +60,7 @@ public class Knight_Movement : MonoBehaviour
                 }
             }
         playAnimation();
+        playCoin(startPos);
     }
 
     private void Inputs()
@@ -126,6 +128,22 @@ public class Knight_Movement : MonoBehaviour
         }
         return false;
     }
+    private bool IsCoin(Vector3 startPos)
+    {
+        GameObject[] coinSprites = GameObject.FindGameObjectsWithTag("Coin");
+        foreach (GameObject coin in coinSprites)
+        {
+            Vector3 p2 = coin.transform.position;
+            float distance = Vector3.Distance(startPos, p2);
+
+            if (distance <= 0.15)
+            {
+                Debug.Log("coin");
+                return true;
+            }
+        }
+        return false;
+    }
 
     private void playAnimation()
     {
@@ -164,9 +182,18 @@ public class Knight_Movement : MonoBehaviour
     }
     private void playAudio()
     {
-        if (!audio.isPlaying)
+        if (!footsteps.isPlaying)
         {
-            audio.Play();
+            footsteps.Play();
+        }
+        
+    }
+    private void playCoin(Vector3 startPos)
+    {
+
+        if (IsCoin(startPos) && !coin.isPlaying && tweener.activeTween != null)
+        {
+            coin.Play();
         }
     }
 }
